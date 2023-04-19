cd7cc5e28ad4a61e8ab8b540e8cbbd5d9bac91c0
Rafal Strzelczyk <rafal.strzelczyk@swisscom.com>
2023-04-19 13:16:28 +0200

    github_secrets

--- vm_check.sh
+++ vm_check.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-
-#Login to Azure with service principal to read keyvault secrets.
-az login --service-principal -u $read_secret_principal_id  -p $read_secret_principal_pw --tenant $tenant
-
 #Variable to get a subscription id
 export sublist="$(az account subscription list --query "[].subscriptionId" -o tsv)"
 
 #Variables to get details of SP
-export SP_ID="$(az keyvault secret show --name $secret_principal_id --vault-name $vault_name --query value  -o tsv)"
-export SP_PW="$(az keyvault secret show --name $secret_principal_pw --vault-name $vault_name --query value -o tsv)"
-export TENANT="$(az keyvault secret show --name $secret_principal_tenant --vault-name $vault_name --query value -o tsv)"
+export SP_ID="$(az keyvault secret show --name ${{ secrets.secret_principal_id }} --vault-name ${{ secrets.vault_name }} --query value  -o tsv)"
+export SP_PW="$(az keyvault secret show --name ${{ secrets.secret_principal_pw }} --vault-name ${{ secrets.vault_name }} --query value -o tsv)"
+export TENANT="$(az keyvault secret show --name ${{ secrets.secret_principal_tetant }} --vault-name ${{ secrets.vault_name }} --query value -o tsv)"
 
 # #Login to Azure using keyvalt secrets
 az login --service-principal -u $SP_ID -p $SP_PW --tenant $TENANT
