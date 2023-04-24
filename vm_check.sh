b55d49c997943831f32202539c68838f030fc694
Rafal Strzelczyk <rafal.strzelczyk@swisscom.com>
2023-04-24 16:08:56 +0200

    update

--- vm_check.sh
+++ vm_check.sh
@@ -13,7 +13,7 @@ export RESOURCE_LIST="$(az resource list --tag gpcms_managed=true --query "[?typ
 # #Login to Azure using keyvalt secrets
 az login --service-principal -u $SP_ID -p $SP_PW --tenant $TENANT
 #Create a CSV file
-echo "NAME,OPERATING SYSTEM,PRIVATE IP ADDRESS,GPCMS_ENV (TAG),SUBSCRIPTION ID" > vm.csv
+echo "NAME,OPERATING SYSTEM,PRIVATE IP ADDRESS,GPCMS_ENV (TAG),SUBSCRIPTION ID" > ./customer_vms.csv
 
 #Get data from Azure
 #For each subscription SP have access to
@@ -24,5 +24,5 @@ az account set --subscription $i
 #Create a VM list with tag gpcms_managed set to true
 export RESOURCE_LIST="$(az resource list --tag gpcms_managed=true --query "[?type=='Microsoft.Compute/virtualMachines'].id" -o tsv)"
 #Add a list of VM with require data to csv
-az vm show -d --ids $RESOURCE_LIST --query "{Name:name, OPERATING_SYSTEM:storageProfile.osDisk.osType, PRIVATE_IP_ADDRESS:privateIps, GPCMS_ENV_TAG:tags.gpcms_env}" -o tsv | sed 's/\t/,/g' | sed "s|$|\,${i}|" >> customer_vms.csv
+az vm show -d --ids $RESOURCE_LIST --query "{Name:name, OPERATING_SYSTEM:storageProfile.osDisk.osType, PRIVATE_IP_ADDRESS:privateIps, GPCMS_ENV_TAG:tags.gpcms_env}" -o tsv | sed 's/\t/,/g' | sed "s|$|\,${i}|" >> ./customer_vms.csv
 done
